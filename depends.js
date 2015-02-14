b17cdf072b9153ecbce0342ee57635672c121c8b
Pooky <martin.klima@aol.com>
2015-02-14 13:17:39 +0100

    First commit

---
+++ depends.js
@@ -0,0 +1,17 @@
+enyo.depends(
+   "source/Tetris.js",
+   "source/Main.js",
+   "source/Shape.js",
+   "source/Shapes.js",
+   "source/Point.js",
+   "source/HighScore.js"   
+);
+// Knihovny
+enyo.depends(
+   "source/database-enyo.js"
+);
+// CSS
+enyo.depends(
+   "source/Tetris.css",
+   "source/HighScore.css"   
+);
